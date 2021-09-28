bc1beee52b301c48ff021acfe03b7bc50d235b43
Caio Peres <caio@nork.digital>
2021-09-28 09:55:05 -0300

    ajustes

--- login-warning.php
+++ login-warning.php
@@ -1,21 +1,39 @@
 <!DOCTYPE>
+
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US">
+
 <head profile="http://gmpg.org/xfn/11">
+
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Login Warning</title>
+
+	<title>ERRO!</title>
+
 	<meta name="robots" content="noodp,noydir" />
+
 	<style>
-	#wrapper {
-		width: 960px;
-		margin: 20px auto;
-	}
+		#wrapper {
+
+			width: 960px;
+
+			margin: 20px auto;
+
+		}
 	</style>
 
+
+
 </head>
+
 <body>
+
 	<div id="wrapper">
-	<h1>Login Warning</h1>
-	<p>There was an issue with your log in. Your user account has logged in recently from a different location.</p>
+
+		<h1>Número de logins máximo atingido!</h1>
+
+		<p>Faça o upgrade do seu plano ou entre em contato com nosso time de suporte.</p>
+
 	</div>
+
 </body>
+
 </html>
